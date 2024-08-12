4866fb2db1662f6008b8d8cbd14606de265d5e4d
Vladislav Sorokin <sorokinvj@gmail.com>
2024-08-12 19:40:50 +0100

    fix: font size

--- app/(public)/schedule-demo/page.tsx
+++ app/(public)/schedule-demo/page.tsx
@@ -4,7 +4,7 @@ export default function ScheduleDemoPage() {
   return (
     <div className="flex flex-col md:flex-row py-8 md:py-16 gap-0 md:gap-40">
       <div className="w-full md:w-1/2">
-        <h1 className="text-4xl mb-4">Schedule a Demo</h1>
+        <h1 className="text-3xl md:text-4xl mb-4">Schedule a Demo</h1>
         <p className="mb-12">
           Take full advantage of ProxyLink&apos;s tools for managing 3rd party
           customer support requests.
@@ -18,7 +18,7 @@ export default function ScheduleDemoPage() {
         ></iframe>
       </div>
       <div className="max-w-[380px] w-full md:w-1/">
-        <h1 className="text-4xl mb-8">Contact Us</h1>
+        <h1 className="text-3xl md:text-4xl mb-8">Contact Us</h1>
         <ContactForm />
       </div>
     </div>
