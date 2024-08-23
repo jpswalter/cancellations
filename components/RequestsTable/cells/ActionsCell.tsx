43d980552f61ef3f76d228d54d9186161f286206
Vladislav Sorokin <sorokinvj@gmail.com>
2024-08-23 20:36:15 +0100

    feat: do not render action buttons

--- components/RequestsTable/cells/ActionsCell.tsx
+++ components/RequestsTable/cells/ActionsCell.tsx
@@ -28,25 +28,49 @@ const ActionsCell: React.FC<ActionsCellProps> = ({ row, toggleDrawer }) => {
 
   const status = row.original.status;
 
-  const isButtonDisabled = (
-    buttonType: 'saveOffer' | 'cancel' | 'decline' | 'confirm',
-  ) => {
-    switch (status) {
-      case 'Pending':
-        return false;
-      case 'Declined':
-        return (
-          buttonType === 'cancel' ||
-          buttonType === 'decline' ||
-          buttonType === 'confirm'
-        );
-      case 'Save Declined':
-        return buttonType === 'saveOffer';
-      case 'Save Accepted':
-        return buttonType !== 'confirm';
-      default:
-        return true;
-    }
+  const renderSaveOfferButton = () => {
+    if (
+      status === 'Save Declined' ||
+      status === 'Canceled' ||
+      status === 'Save Offered' ||
+      status === 'Save Confirmed'
+    )
+      return null;
+    return (
+      <Button onClick={handleSaveOfferClick} color="blue" className="w-24">
+        {status === 'Save Accepted' ? 'Confirm' : 'Save Offer'}
+      </Button>
+    );
+  };
+
+  const renderCancelButton = () => {
+    if (
+      status === 'Declined' ||
+      status === 'Save Accepted' ||
+      status === 'Canceled' ||
+      status === 'Save Confirmed'
+    )
+      return null;
+    return (
+      <Button onClick={() => openResolveModal('cancel')} color="teal">
+        Cancel
+      </Button>
+    );
+  };
+
+  const renderDeclineButton = () => {
+    if (
+      status === 'Declined' ||
+      status === 'Save Accepted' ||
+      status === 'Canceled' ||
+      status === 'Save Confirmed'
+    )
+      return null;
+    return (
+      <Button onClick={() => openResolveModal('decline')} color="red">
+        Decline
+      </Button>
+    );
   };
 
   const handleSaveOfferClick = () => {
@@ -60,30 +84,9 @@ const ActionsCell: React.FC<ActionsCellProps> = ({ row, toggleDrawer }) => {
   return (
     <div onClick={e => e.stopPropagation()}>
       <div className="flex space-x-2 justify-end">
-        <Button
-          onClick={handleSaveOfferClick}
-          disabled={isButtonDisabled(
-            status === 'Save Accepted' ? 'confirm' : 'saveOffer',
-          )}
-          color="blue"
-          className="w-24"
-        >
-          {status === 'Save Accepted' ? 'Confirm' : 'Save Offer'}
-        </Button>
-        <Button
-          onClick={() => openResolveModal('cancel')}
-          disabled={isButtonDisabled('cancel')}
-          color="teal"
-        >
-          Cancel
-        </Button>
-        <Button
-          onClick={() => openResolveModal('decline')}
-          disabled={isButtonDisabled('decline')}
-          color="red"
-        >
-          Decline
-        </Button>
+        {renderSaveOfferButton()}
+        {renderCancelButton()}
+        {renderDeclineButton()}
       </div>
 
       <SaveOfferModal
